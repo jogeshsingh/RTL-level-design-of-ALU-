9b8c5315d781eb7a4a7ffe278309c096960bf111
Jogesh Singh <singhjogesh865@gmail.com>
2022-09-04 11:05:20 +0530

    Update DA_CU.v

--- Project_files/DA_CU.v
+++ Project_files/DA_CU.v
@@ -21,6 +21,24 @@
 
 
 module DA_CU(
-
+clk, rst_n , data_in , data_out , REG1, REG2 , state , start
     );
+ input clk , rst_n, start;
+ output  [2:0] state ;
+ input [3:0] data_in ;
+ output [3:0] REG1 , REG2  ;
+ output [3:0] data_out ;   
+ wire ld_1 , ld_2 , sel_1 , op , en ;   
+  
+ data_path DATAPATH(
+clk , rst_n , sel_1 , op  , REG1 , REG2 , ld_1  , ld_2 , data_in , data_out, en
+    ); 
+    
+ controller CONTROL_PATH(
+clk, rst_n , ld_1, ld_2 , sel_1 , op , en , state, start 
+    );
+    
+  
+  
+    
 endmodule
